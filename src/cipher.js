c119c1ee3315103808ff5a5eb3d7a7edc9e621e8
Erika Vidal <erika.diana.vidal@gmail.com>
2019-03-04 17:23:59 -0500

    Cifrado/Descifrado

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,16 @@
 window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
-  },
-  decode: () => {
-    /* Acá va tu código */
+  encode: (string,offset) => {
+    let msjE="";
+    for(let i=0;i<string.length;i++){
+      msjE+=String.fromCharCode((string.charCodeAt(i)-65+offset)%26+65);
+    } return msjE;
+  }
+,
+  decode: (string,offset) => {
+  let msjD="";
+    for(let i=0;i<string.length;i++){
+      msjD+=String.fromCharCode((string.charCodeAt(i)-90-offset)%26+90);
+    } return msjD;
+
   }
 };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-/* Acá va tu código */
+    document.getElementById("btn_encriptar").addEventListener("click",()=>{
+    const string = document.getElementById("string").value;
+    const offset = parseInt(document.getElementById("offset").value);
+    const resultado = document.getElementById("resultado");
+    resultado.innerHTML = cipher.encode(string,offset);
+});
+document.getElementById("btn_desencriptar").addEventListener("click",()=>{
+    const string = document.getElementById("string").value;
+    const offset = parseInt(document.getElementById("offset").value);
+    const resultado = document.getElementById("resultado");
+    resultado.innerHTML=cipher.decode(string,offset);
+});
+
